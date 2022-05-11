39f0940439da96762fb65b91b30996ffb92cdc91
rahulsahay19 <rahulsahay19@hotmail.com>
2022-05-11 14:00:13 +0530

    Initial commit

---
+++ src/main/java/myview/springframework/rahulbrewery/RahulBreweryApplication.java
@@ -0,0 +1,13 @@
+package myview.springframework.rahulbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahulBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RahulBreweryApplication.class, args);
+    }
+
+}
---
+++ src/main/java/myview/springframework/rahulbrewery/web/model/BeerDto.java
@@ -0,0 +1,21 @@
+package myview.springframework.rahulbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+//Lombok will provide data --> getter and setter, equal and hashCode methods, no Args ctor, All args ctor and Builder pattern as well
+//With Lombok, one can save ton of typings
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+}
